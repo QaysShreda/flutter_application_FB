a0e3e3ab93ea0811fa2153044869002084c5af34
qaysshreda <qays.shreda@gmail.com>
2022-12-04 15:34:25 +0200

    init5

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,61 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_application_1/auth/auth_helper.dart';
 
 class HomeScreen extends StatelessWidget {
-  TextEditingController mailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("FireBase"),
-      ),
-      body: Form(
-        key: formKey,
-        child: Column(children: [
-          TextFormField(
-            validator: (v) {
-              if (v == null) {
-                return "erquired field";
-              } else if (!(v.contains('@'))) {
-                return 'Incorrect email';
-              }
-            },
-            controller: mailController,
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), hintText: 'Enter Your Email'),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          TextFormField(
-            validator: (v) {
-              if (v == null) {
-                return "required field";
-              } else if (v.length  <= 6) {
-                return 'Error, the password should be larger than 6 letters';
-              }
-            },
-            controller: passwordController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Password',
-            ),
-          ),
-          OutlinedButton(
-              onPressed: () {
-                bool isValid = formKey.currentState!.validate();
-                if (isValid) {
-                  AuthHelper.authHelper
-                      .signUp(mailController.text, passwordController.text);
-                }
-              },
-              child: Text('Save'))
-        ]),
+        title: Text("Home Screen"),
       ),
     );
   }
